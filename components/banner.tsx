7902727c6a30c2002a4d9598d9c09d773a902b84
Komil-Muminov <141549753+Komil-Muminov@users.noreply.github.com>
2023-10-30 15:55:29 +0500

    commented

--- components/banner.tsx
+++ components/banner.tsx
@@ -1,13 +1,13 @@
-'use client'
+"use client";
 
-import { useState } from 'react'
+// import { useState } from 'react'
 
 export default function Banner() {
-  const [bannerOpen, setBannerOpen] = useState<boolean>(true)
+	// const [bannerOpen, setBannerOpen] = useState<boolean>(true)
 
-  return (
-    <>
-      {bannerOpen && (
+	return (
+		<>
+			{/* {bannerOpen && (
         <div className="fixed bottom-0 right-0 w-full md:bottom-8 md:right-12 md:w-auto z-50">
           <div className="bg-slate-800 text-slate-50 text-sm p-3 md:rounded shadow-lg flex justify-between">
             <div className="text-slate-500 inline-flex">
@@ -31,8 +31,8 @@ export default function Banner() {
               </svg>
             </button>
           </div>
-        </div>
-      )}
-    </>
-  )
+        </div> */}
+			{/* )} */}
+		</>
+	);
 }

a5dd6cbe6357f52db6c024eaeaf5a88b21cae94a
Komil-Muminov <141549753+Komil-Muminov@users.noreply.github.com>
2023-11-04 14:39:33 +0500

    created component TestimonMore

--- components/Navigation.jsx
+++ components/Navigation.jsx
@@ -25,7 +25,6 @@ const Navigation = () => {
 								<path d="M20 24h-8c-2.056 0-4-1.944-4-4v-8c0-2.056 1.944-4 4-4h8c2.056 0 4 1.944 4 4v8c0 2.056-1.944 4-4 4zm-8-14c-.935 0-2 1.065-2 2v8c0 .953 1.047 2 2 2h8c.935 0 2-1.065 2-2v-8c0-.935-1.065-2-2-2h-8z" />
 							</svg>
 						</Link>
-
 						<Link
 							href="/"
 							className="flex justify-center items-center text-purple-600 bg-gray-800 hover:text-gray-100 hover:bg-purple-600 rounded-full transition duration-150 ease-in-out"
---
+++ components/testimonMore.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+
+const testimonMore = () => {
+	return (
+		<>
+			<section className="testimonials-more-page" id="testimonMore">
+				<div className="more-page-container">
+					<div
+						className="flex flex-col h-full p-6 bg-gray-800"
+						data-aos="fade-up"
+					>
+						<div>
+							<div className="flex flex-col mb-4">
+								<div className="person-wrapper flex items-center  gap-3">
+									<Image
+										className="rounded-full"
+										src={TestimonialImage01}
+										width={48}
+										height={48}
+										alt="Testimonial 01"
+									/>
+									<cite className="text-gray-200 not-italic">
+										Anastasia Dan
+									</cite>{" "}
+								</div>
+							</div>
+						</div>
+						<blockquote className="text-lg text-gray-400 grow">
+							—Lorem ipsum dolor sit amet consectetur adipisicing elit.
+							Accusantium mollitia corporis nesciunt, hic delectus modi
+							reiciendis commodi est quae sunt similique quis molestias
+							quibusdam illum quia ratione repellat ipsum expedita!
+						</blockquote>
+						<div className="text-gray-700 font-medium mt-6 pt-5 border-t border-gray-700">
+							-{" "}
+							<a
+								className="text-purple-600 hover:text-gray-200 transition duration-150 ease-in-out"
+								href="#0"
+							>
+								Подробнее
+							</a>
+						</div>
+					</div>
+				</div>
+			</section>
+		</>
+	);
+};
+
+export default testimonMore;
